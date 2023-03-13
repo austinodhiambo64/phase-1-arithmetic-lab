e851d2384cde5aa6b0ad704ed4aaebe983781c12
Austin <austinodhiambo64@gmail.com>
2023-03-14 00:18:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 24;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 100) + 1;
+console.log(random); // output: a random integer between 1 and 100
+let num4 = 20
+let mod = num3 % 5;
+console.log(mod); // output: 4
+let max = Math.max(1, 5, 10, 20, 3);
+console.log(max); // output: 20
